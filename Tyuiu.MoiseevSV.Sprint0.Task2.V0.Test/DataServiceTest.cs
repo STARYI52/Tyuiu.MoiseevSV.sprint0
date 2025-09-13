dfd885bde3141d4e1b41de3afbecf01b895098c1
stepan moiseev <stepanmoiseev83@gmail.com>
2025-09-13 14:12:24 +0500

    Task2

---
+++ Tyuiu.MoiseevSV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MoiseevSV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MoiseevSV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheakGetMassageValid()
+        {
+            var name = "Степан";
+            var res = DataService.GetMassage(name);
+            Assert.AreEqual("Привет, Степан", res);
+        }
+    }
+}
---
+++ Tyuiu.MoiseevSV.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MoiseevSV.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MoiseevSV.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd87ac10-17ad-4137-8531-5f5779369873")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
